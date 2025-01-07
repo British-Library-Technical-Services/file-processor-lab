2c8b55ba90bbf0f5fff674060018e5016163cad9
British Library Sound and Vision <66015813+British-Library-Technical-Services@users.noreply.github.com>
2025-01-07 09:56:55 +0000

    Add glob dependency and implement file listing for checksum generation

--- rust/checksum_code/src/main.rs
+++ rust/checksum_code/src/main.rs
@@ -1,35 +1,50 @@
 use std::time::Instant;
 use std::fs::File;
-use std::io::{Read, BufReader};
+use std::io::{BufReader, Read};
+
 use sha2::{Sha256, Digest};
+use glob::glob;
 
-// 1:1 checksum generation
-fn generate_file_checksum(filepath: &str) -> Result<String, std::io::Error> {
-    let start = Instant::now();
+fn get_file_list(pattern: &str) -> Vec<String>  {
+    let mut file_list = Vec::new();
+    for entry in glob(pattern).expect("Failed to read glob pattern") {
+        match entry {
+            Ok(path) => {
+                file_list.push(path.display().to_string());
+            },
+            Err(e) => { 
+                println!("{:?}", e)
+            }
+        }
+    }
+    file_list
+}
 
-    let file = File::open(filepath)?;
+fn generate_checksum(file_path: &str) -> String {
+    let file = File::open(file_path).expect("Failed to open file");
     let mut reader = BufReader::new(file);
     let mut hasher = Sha256::new();
     let mut buffer = [0; 1024];
-    
     loop {
-        let bytes_read = reader.read(&mut buffer)?;
-        if bytes_read == 0 {
+        let n = reader.read(&mut buffer).expect("Failed to read file");
+        if n == 0 {
             break;
+        } else {
+            hasher.update(&buffer[..n]);
         }
-        hasher.update(&buffer[0..bytes_read]);
     }
-
-    let hash = hasher.finalize();
-    let duration = start.elapsed();
-    println!("{:?}", duration);
-    Ok(format!("{:x}", hash))
+    let result = hasher.finalize();
+    format!("{:x}", result)
 }
 
 fn main() {
-    match generate_file_checksum("test_file.dpx") {
-        Ok(checksum) => println!("{}", checksum),
-        Err(e) => eprintln!("{}", e),
-    }
-}
+    let file_path = "/workspaces/file-processor-lab/_test_files/*.wav";
+    let files = get_file_list(file_path);
+    let start = Instant::now();
 
+    for file in files {
+        let checksum = generate_checksum(&file);
+        println!("{}: {}", file, checksum);
+    }
+    println!("Elapsed time: {:?}", start.elapsed());
+}
